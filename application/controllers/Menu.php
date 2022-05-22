c33ddc98eb0ec28945350318c28113ddf0f7cb07
MuhammadEdwar <edwarmhmd123@gmail.com>
2022-05-22 16:38:15 +0800

    Pertemuan Ketiga

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -2,11 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Menu extends CI_Controller {
-	public function index()
-	{
-         $data = array(
-			 'content'=> 'dashboard/index.php'
-		 )
-		$this->load->view('template/menu',$data);
+	public function index() {
+		$this->load->view('template/menu');
 	}
 }
